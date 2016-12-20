d51ba5d26245acfcd182c44318f02c81bd8c58ad
Jay P. Hayes <jayp3@jayp.com>
2016-12-20 15:56:50 -0600

    Anular Fire updates. Kool

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {initializeApp, database} from 'firebase';
 import {firebaseConfig} from '../environments/firebase.config';
 import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -11,19 +12,50 @@ import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'ang
 export class AppComponent {
   title = 'App Works with GitHub!';
 
+  courses$: FirebaseListObservable<any>;
+  lessons$: FirebaseObjectObservable<any>;
+  firstCourse: any;
+
   constructor(private af: AngularFire){
+    this.courses$ = af.database.list('courses');
 
-    const courses$ : FirebaseListObservable<any> = af.database.list('courses');
-    const course$ = af.database.object('courses/-KZSccjicfsL8RyBkzsq');
+    this.courses$.subscribe(console.log);
+    
+    this.lessons$ = af.database.object('lessons/-KZSccjnA0Sbd2o-Gz-5');
+    this.lessons$.subscribe(console.log);
 
-    course$.subscribe(console.log)
+    this.courses$.map(courses => courses[0]).subscribe(course => this.firstCourse = course);
 
-    // courses$.subscribe(console.log);
-    // courses$.subscribe(
-    //   val => console.log(val)
-    // );
-
+  
     
   }
 
+  listPush(){
+    this.courses$.push({description: 'TEST NEW COURSE'})
+      .then(
+        () => console.log('List Push Done'),
+        console.error
+      );
+  }
+
+  listRemove(){
+    this.courses$.remove(this.firstCourse);
+  }
+
+  listUpdate(){
+    this.courses$.update(this.firstCourse, {description: 'Angular 2 HTTP Modified!'})
+  }
+  objUpdate(){
+
+    this.lessons$.update({description: 'NEW DESCRIPTION'});
+  }
+
+  objSet(){
+    this.lessons$.set({description: 'NEW DESCRIPTION SET'});
+  }
+
+  objRemove(){
+    this.lessons$.remove();
+  }
+    
 }
